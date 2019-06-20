7a900df9f2225895b32f9030dfdddff0c9acaa60
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-06-20 11:41:43 -0400

    Added SkyAppLinkModule

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -3,12 +3,17 @@ import {
 } from '@angular/core';
 
 import {
+  SkyAppLinkModule
+} from '@skyux/router';
+
+import {
   SkyDropdownModule,
   SkyPopoverModule
 } from './public';
 
 @NgModule({
   exports: [
+    SkyAppLinkModule,
     SkyDropdownModule,
     SkyPopoverModule
   ]
