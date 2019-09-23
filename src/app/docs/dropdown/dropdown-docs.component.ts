0b2a6d113e73a33886080d98c215f82fb7992efe
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-09-23 12:51:59 -0400

    Upgrade deps

---
+++ src/app/docs/dropdown/dropdown-docs.component.ts
@@ -0,0 +1,11 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-docs',
+  templateUrl: './dropdown-docs.component.html'
+})
+export class DropdownDocsComponent {
+  public demoSettings: any = {};
+}
