ba66cb01fc1d08e13c387eeea85fc87b9fbe9795
Alex <alex.kingman@blackbaud.com>
2019-10-09 15:08:38 -0400

    Prevent dropdown from going fullscreen (#36)
    * prevent dropdown from going fullscreen
    
    * changed variable to containOverflow. added unit test
    
    * updated dropdown visual tests. added test for contain overflow
    
    * typo
    
    * reverted visual changes to ensure tests have a common baseline for this particular change
    
    * hiding horizontal scrollbar
    
    * added min-height
    
    * changed containOverflow to an Input() property called allowFullscreen
    
    * added setter for allowFullscreen
    
    * allowFullScreen always defaults to true

--- e2e/dropdown.e2e-spec.ts
+++ e2e/dropdown.e2e-spec.ts
@@ -9,6 +9,7 @@ describe('Dropdown', () => {
   it('should match dropdown button screenshot when closed', (done) => {
     SkyHostBrowser.get('visual/dropdown');
     SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-dropdown-button');
     expect('#screenshot-dropdown-button').toMatchBaselineScreenshot(done, {
       screenshotName: 'dropdown-button-closed'
     });
@@ -17,6 +18,7 @@ describe('Dropdown', () => {
   it('should match dropdown button screenshot when open', (done) => {
     SkyHostBrowser.get('visual/dropdown');
     SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-dropdown-button');
     element(by.css('#screenshot-dropdown-button .sky-dropdown-button')).click();
     expect('#screenshot-dropdown-button').toMatchBaselineScreenshot(done, {
       screenshotName: 'dropdown-button-open'
@@ -26,6 +28,7 @@ describe('Dropdown', () => {
   it('should match dropdown context menu screenshot when closed', (done) => {
     SkyHostBrowser.get('visual/dropdown');
     SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-dropdown-context-menu');
     expect('#screenshot-dropdown-context-menu').toMatchBaselineScreenshot(done, {
       screenshotName: 'dropdown-context-menu-closed'
     });
@@ -34,6 +37,7 @@ describe('Dropdown', () => {
   it('should match dropdown context menu screenshot when open', (done) => {
     SkyHostBrowser.get('visual/dropdown');
     SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-dropdown-context-menu');
     element(by.css('#screenshot-dropdown-context-menu .sky-dropdown-button')).click();
     expect('#screenshot-dropdown-context-menu').toMatchBaselineScreenshot(done, {
       screenshotName: 'dropdown-context-menu-open'
@@ -43,9 +47,20 @@ describe('Dropdown', () => {
   it('should match dropdown screenshot when before a relative element', (done) => {
     SkyHostBrowser.get('visual/dropdown');
     SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-dropdown-z-index');
     element(by.css('#screenshot-dropdown-z-index .sky-dropdown-button')).click();
     expect('#screenshot-dropdown-z-index').toMatchBaselineScreenshot(done, {
       screenshotName: 'dropdown-z-index'
+    });
+  });
+
+  it('should match dropdown screenshot when menu items overflow dropdown', (done) => {
+    SkyHostBrowser.get('visual/dropdown');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-dropdown-max-height');
+    element(by.css('#screenshot-dropdown-max-height .sky-dropdown-button')).click();
+    expect('#screenshot-dropdown-max-height').toMatchBaselineScreenshot(done, {
+      screenshotName: 'dropdown-max-height'
     });
   });
 });
