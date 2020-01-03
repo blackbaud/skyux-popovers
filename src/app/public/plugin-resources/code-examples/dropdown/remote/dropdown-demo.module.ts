77ff731da6f4b8d50ac1d84c8d69ae21b58db538
Somalia Jamall <somalia.jamall@blackbaud.com>
2020-01-03 12:43:19 -0600

    Copy and convert phase 1 dropdown documentation to phase 2 Add new dropdown demos

---
+++ src/app/public/plugin-resources/code-examples/dropdown/remote/dropdown-demo.module.ts
@@ -0,0 +1,73 @@
+import {
+  ChangeDetectorRef,
+  Component
+} from '@angular/core';
+
+import {
+  SkyDropdownMessage,
+  SkyDropdownMessageType,
+  SkyDropdownMenuChange
+} from '@skyux/popovers';
+
+import {
+  Subject
+} from 'rxjs/Subject';
+
+@Component({
+  selector: 'sky-dropdown-demo',
+  templateUrl: './dropdown-demo.component.html'
+})
+export class SkyDropdownDemoComponent {
+  public dropdownController = new Subject<SkyDropdownMessage>();
+  public items: any[] = [
+    { name: 'Option 1', disabled: false },
+    { name: 'Disabled option', disabled: true },
+    { name: 'Option 3', disabled: false },
+    { name: 'Option 4', disabled: false },
+    { name: 'Option 5', disabled: false }
+  ];
+
+  constructor(
+    private changeDetector: ChangeDetectorRef
+  ) { }
+
+  public actionClicked(action: string) {
+    alert('You selected ' + action);
+  }
+
+  public openDropdown() {
+    this.sendMessage(SkyDropdownMessageType.Open);
+  }
+
+  public closeDropdown() {
+    this.sendMessage(SkyDropdownMessageType.Close);
+  }
+
+  public focusTriggerButton() {
+    this.sendMessage(SkyDropdownMessageType.FocusTriggerButton);
+  }
+
+  public focusNextItem() {
+    this.sendMessage(SkyDropdownMessageType.FocusNextItem);
+  }
+
+  public focusPreviousItem() {
+    this.sendMessage(SkyDropdownMessageType.FocusPreviousItem);
+  }
+
+  public changeItems() {
+    this.items.pop();
+    this.changeDetector.detectChanges();
+  }
+
+  public onMenuChanges(change: SkyDropdownMenuChange) {
+    if (change.activeIndex !== undefined) {
+      console.log('The menu\'s active index changed to:', change.activeIndex);
+    }
+  }
+
+  private sendMessage(type: SkyDropdownMessageType) {
+    const message: SkyDropdownMessage = { type };
+    this.dropdownController.next(message);
+  }
+}
