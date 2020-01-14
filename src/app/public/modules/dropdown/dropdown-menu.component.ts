67ccf38daca8f46bbd2625a5ada2b56695b5f2d4
John Lyons <John.Lyons@blackbaud.com>
2020-01-14 11:18:42 -0500

    Remove types from descriptions

--- src/app/public/modules/dropdown/dropdown-menu.component.ts
+++ src/app/public/modules/dropdown/dropdown-menu.component.ts
@@ -46,7 +46,6 @@ export class SkyDropdownMenuComponent implements AfterContentInit, OnDestroy {
   /**
    * Sets the dropdown menu's `aria-labelledby` attribute to support accessibility. The value should
    * be the HTML element ID (without the leading `#`) of the element that labels the dropdown menu.
-   * This property accepts `string` values.
    */
   @Input()
   public ariaLabelledBy: string;
@@ -55,7 +54,7 @@ export class SkyDropdownMenuComponent implements AfterContentInit, OnDestroy {
    * Specifies an ARIA role for the dropdown menu to support accessibility by indicating how the
    * dropdown menu functions and what it controls. The ARIA role indicates what the dropdown menu
    * represents on the web page. For information about ARIA roles, see the
-   * [WAI-ARIA roles model](https://www.w3.org/WAI/PF/aria/roles). This property accepts `string` values.
+   * [WAI-ARIA roles model](https://www.w3.org/WAI/PF/aria/roles).
    */
   @Input()
   public ariaRole = 'menu';
@@ -64,7 +63,7 @@ export class SkyDropdownMenuComponent implements AfterContentInit, OnDestroy {
    * Indicates whether to use the browser's native focus function when users navigate through menu
    * items with the keyboard. To disable the native focus function, set this property to `false`.
    * For example, to let users interact with the dropdown menu but keep the keyboard focus on a
-   * different element, set this property to `false`. This property accepts `boolean` values.
+   * different element, set this property to `false`.
    */
   @Input()
   public useNativeFocus = true;
