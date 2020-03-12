bb5b85a87c38867051ed91aecb6b6aa4f19b0b3b
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-03-12 15:59:55 -0400

    Disable animations for visual

--- src/app/visual/popover/popover-visual.component.ts
+++ src/app/visual/popover/popover-visual.component.ts
@@ -1,10 +1,29 @@
 import {
-  Component
+  AfterViewInit,
+  Component,
+  QueryList,
+  ViewChildren
 } from '@angular/core';
+
+import {
+  SkyPopoverComponent
+} from '../../public';
 
 @Component({
   selector: 'popover-visual',
   templateUrl: './popover-visual.component.html',
   styleUrls: ['./popover-visual.component.scss']
 })
-export class PopoverVisualComponent { }
+export class PopoverVisualComponent implements AfterViewInit {
+
+  @ViewChildren(SkyPopoverComponent)
+  private popoverComponents: QueryList<SkyPopoverComponent>;
+
+  public ngAfterViewInit(): void {
+    // Disable animations for the visual tests.
+    setTimeout(() => {
+      this.popoverComponents.forEach(c => c.enableAnimations = false);
+    });
+  }
+
+}
