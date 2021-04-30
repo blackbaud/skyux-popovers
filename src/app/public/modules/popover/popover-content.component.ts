cfbc2a6dd3ae6be294cd8fcb8602b42fe4ad4d86
Trevor Burch <Trevor.Burch@blackbaud.com>
2021-04-30 14:22:46 -0400

    Fixed timing issue with popovers

--- src/app/public/modules/popover/popover-content.component.ts
+++ src/app/public/modules/popover/popover-content.component.ts
@@ -263,7 +263,7 @@ export class SkyPopoverContentComponent implements OnInit, OnDestroy {
 
   public close(): void {
     this.isOpen = false;
-    this.changeDetector.markForCheck();
+    this.changeDetector.detectChanges();
   }
 
   public applyFocus(): void {
