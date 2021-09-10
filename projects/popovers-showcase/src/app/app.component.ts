174c6f149be17692dee591e2f04276208a95d3be
Braden Bisping <bradenneilbisping@tutanota.com>
2021-09-10 10:46:27 -0500

    changes

--- projects/popovers-showcase/src/app/app.component.ts
+++ projects/popovers-showcase/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import {
-  Component
+  Component,
+  Renderer2
 } from '@angular/core';
 
 import {
-  SkyAppStyleLoader
+  SkyAppStyleLoader,
+  SkyTheme,
+  SkyThemeMode,
+  SkyThemeService,
+  SkyThemeSettings
 } from '@skyux/theme';
 
 @Component({
@@ -16,8 +21,19 @@ export class AppComponent {
   public isLoaded = false;
 
   constructor(
+    renderer: Renderer2,
+    themeService: SkyThemeService,
     styleLoader: SkyAppStyleLoader
   ) {
+    themeService.init(
+      document.body,
+      renderer,
+      new SkyThemeSettings(
+        SkyTheme.presets['default'],
+        SkyThemeMode.presets.light
+      )
+    );
+
     styleLoader.loadStyles().then(() => {
       this.isLoaded = true;
     });
